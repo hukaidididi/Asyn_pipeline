db88a97b1c6eca0bb0b77711ddbb46b7dc6d6d66
hukaidididi <44676210+hukaidididi@users.noreply.github.com>
2019-06-02 20:41:33 +0800

    Update stage_6combind.v

--- stage_6combind.v
+++ stage_6combind.v
@@ -6,26 +6,36 @@ module stage_6combind(req_in,ack_in,data_in,rst,req_out,ack_out,data_out);
   output req_out;
   output ack_out;
   output [2:0] data_out;
-  wire temp_ack,temp_req;
-  wire [2:0] temp_data;
+  wire req_in_wire,ack_in_wire,req_out_wire,ack_out_wire;
+  wire [2:0] data_in_wire,data_out_wire;
   
   stage_3combind a1(
     .req_in(req_in),
-    .ack_in(temp_ack),
+    .ack_in(ack_out_wire),
     .data_in(data_in),
     .rst(rst),
-    .req_out(temp_req),
+    .req_out(req_in_wire),
     .ack_out(ack_out),
-    .data_out(temp_data)
+    .data_out(data_in_wire)
+  );
+  
+  stall_3_1 s31(
+    .temp_req_in(req_in_wire),
+    .temp_ack_in(ack_in_wire),
+    .temp_data_in(data_in_wire),
+    .temp_req_out(req_out_wire),
+    .temp_ack_out(ack_out_wire),
+    .temp_data_out(data_out_wire)
   );
   
   stage_3combind a2(
-    .req_in(temp_req),
+    .req_in(req_out_wire),
     .ack_in(ack_in),
-    .data_in(temp_data),
+    .data_in(data_out_wire),
     .rst(rst),
     .req_out(req_out),
-    .ack_out(temp_ack),
+    .ack_out(ack_in_wire),
     .data_out(data_out)
   );
 endmodule
+
